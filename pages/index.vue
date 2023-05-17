25de5b358d9fba15abbf5708d8492f11b789ef3a
Yurii Rybachok <drummer2009@ukr.net>
2023-05-17 11:59:32 +0300

    add data to Home page

--- pages/index.vue
+++ pages/index.vue
@@ -7,7 +7,16 @@
 
 <script>
   export default {
-
+      async created() {
+        this.sale_items = await this.$content('products').where({onSale: true}).fetch();
+        this.products = await this.$content('products').fetch();
+      },
+      data() {
+        return {
+          products: null,
+          sale_items: null,
+        }
+      },
   }
 </script>
 
