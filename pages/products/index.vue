05b8751ea296722a15551c8652296f07c363f651
Yurii Rybachok <drummer2009@ukr.net>
2023-05-17 16:27:28 +0300

    create Products page & serch for him

---
+++ pages/products/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <Nav />
+    <v-container>
+      <v-row dense>
+        <v-col md="3">
+          <div>
+            <v-text-field prepend-inner-icon="mdi-magnify" outlined clearable placeholder="Search" />
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style>
+
+</style>
