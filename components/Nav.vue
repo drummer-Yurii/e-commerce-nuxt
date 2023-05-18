30005dddda7e535024cdffc3610b5456320e4223
Yurii Rybachok <drummer2009@ukr.net>
2023-05-18 20:18:22 +0300

    create cart page

--- components/Nav.vue
+++ components/Nav.vue
@@ -19,11 +19,11 @@
         :content="$store.state.cart.cart.length"
         v-if="$store.state.cart.cart.length > 0"
       >
-        <v-btn icon>
+        <v-btn nuxt to="/cart" icon>
           <v-icon size="20">mdi-cart-outline</v-icon>
         </v-btn>
       </v-badge>
-      <v-btn v-else icon>
+      <v-btn nuxt to="/cart" v-else icon>
         <v-icon size="20">mdi-cart-outline</v-icon>
       </v-btn>
       <v-divider vertical class="mx-md-5 mx-2" />
